9432c0b5978c6d119e111911418983343f9bd4ee
thanosks <72318224+thanosks@users.noreply.github.com>
2023-03-15 13:49:18 +0200

    Update filter.lua

--- filter.lua
+++ filter.lua
@@ -1,12 +1,14 @@
-function Filter(images)
-    if images.classes:find('filter',1) then
-      local f = io.open("./Kallipos-Notes-LetMeDoItForYou" .. fn .. ".md", 'r')
-      local doc = pandoc.read(f:read('*a'))
-      f:close()
-      local caption = pandoc.utils.stringify(doc.meta.caption) 
-      local student = pandoc.utils.stringify(doc.meta.student)
-      local am = pandoc.utils.stringify(doc.meta.AM)
-      local content = "> " .. caption .. "  \n>" .. " Ονοματεπώνυμο:" .. student .. "  \n>" .. " Αριθμός Μητρώου:" .. am
-      return pandoc.RawInline('markdown',content)
-    end
+function Image(img)
+  if img.classes:find('filter',1) then
+    local f = io.open("Kallipos-Notes-LetMeDoItForYou/" .. img.src, 'r')
+    local doc = pandoc.read(f:read('*a'))
+    f:close()
+    local addition = pandoc.utils.stringify(doc.meta.caption)
+    local student = pandoc.utils.stringify(doc.meta.student)
+    local am = pandoc.utils.stringify(doc.meta.AM)
+    content = content .. ">_" .. addition .. "_\n>"
+    content = content .. ">" .. student .. "\n\n"
+    content = content .. ">" .. am .. " \n>"
+    return pandoc.RawInline('markdown',content)
+  end
 end
