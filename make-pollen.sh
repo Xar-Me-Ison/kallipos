f16340d7e67c77f8b222a8f18b55ffa2247d479c
Χάρης Μακρυλάκης <79524490+Xar-Me-Ison@users.noreply.github.com>
2023-04-09 11:37:25 +0300

    Update make-pollen.sh

--- make-pollen.sh
+++ make-pollen.sh
@@ -2,10 +2,10 @@
 FONT="Open Sans"
 
 if [ ! -d html ]; then
-  echo "Directory does not exist. Creating latex..."
+  echo "Directory does not exist. Creating HTML directory..."
   mkdir html
 else
-  echo "Latex directory already exists."
+  echo "HTML directory already exists."
 fi
 
 echo "Remaking the book directory..."
@@ -19,7 +19,7 @@ pandoc text/intro.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-lev
 
 for filename in text/ch*.txt; do
    [ -e "$filename" ] || continue
-   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=filter.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex"
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=filter.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to html > html/"$(basename "$filename" .txt).html"
 done
 
 pandoc text/web.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to html > html/web.html
@@ -27,7 +27,7 @@ pandoc text/bio.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level
 
 for filename in text/apx*.txt; do
    [ -e "$filename" ] || continue
-   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to html > html/"$(basename "$filename" .txt).html"
 done
 
 echo "Merging html files... "
@@ -38,7 +38,7 @@ pandoc -N --quiet --variable "geometry=margin=1.2in" --variable mainfont="$FONT"
 echo "Finished converting to pdf. "
 
 echo "Converting to epub..."
-pandoc -o book/book.epub book/book.tex --metadata title="book"
+pandoc -o book/book.epub book/book.html --metadata title="book"
 
 echo "Converting to html..."
 echo "#lang pollen" >> book/book.html.pmd
