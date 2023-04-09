bec4aee13d2324d9caff0e18e9a9f501f9747166
Χάρης Μακρυλάκης <79524490+Xar-Me-Ison@users.noreply.github.com>
2023-04-09 11:32:35 +0300

    Update make-pollen.sh

--- make-pollen.sh
+++ make-pollen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 FONT="Open Sans"
 
-if [ ! -d latex ]; then
+if [ ! -d html ]; then
   echo "Directory does not exist. Creating latex..."
-  mkdir latex
+  mkdir html
 else
   echo "Latex directory already exists."
 fi
@@ -14,24 +14,24 @@ mkdir book
 
 echo "Assembling and preprocessing all the sources files..."
 
-pandoc text/pre.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/pre.tex
-pandoc text/intro.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/intro.tex
+pandoc text/pre.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to html > html/pre.html
+pandoc text/intro.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to html > html/intro.html
 
 for filename in text/ch*.txt; do
    [ -e "$filename" ] || continue
    pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=filter.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
-pandoc text/web.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/web.tex
-pandoc text/bio.txt --top-level-division=chapter --to latex > latex/bio.tex
+pandoc text/web.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to html > html/web.html
+pandoc text/bio.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to html > html//bio.html
 
 for filename in text/apx*.txt; do
    [ -e "$filename" ] || continue
    pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
-echo "Merging tex files... "
-pandoc -s latex/*.tex -o book/book.tex
+echo "Merging html files... "
+pandoc --quiet -s html/*.html -o index.html --metadata title="Ο Προγραμματισμός της Διάδρασης"
 
 echo "Converting to pdf... "
 pandoc -N --quiet --variable "geometry=margin=1.2in" --variable mainfont="$FONT" --variable sansfont="$FONT" --variable monofont="$FONT" --variable fontsize=12pt --variable version=2.0 book/book.tex  --pdf-engine=xelatex --toc -o book/book.pdf
